586ea996da2bb83b50718db8e1d6423fccec989e
Kaio Soledade <soledadekaio@gmail.com>
2023-05-24 21:50:46 -0300

    adição de JS para imprimir infomação

---
+++ age-calculator-app-main/index.js
@@ -0,0 +1,52 @@
+
+function submeter(){
+    var n1 = window.document.querySelector('.imprimir1').innerHTML
+    var n1 = window.document.querySelector('.imprimir1').innerHTML
+    var n1 = window.document.querySelector('.imprimir1').innerHTML
+
+}
+
+ /*function {
+    var n1 = window.document.querySelector('.imprimir1').innerHTML
+    var n2 = window.document.querySelector('.meses')
+    var n3 = window.document.querySelector('.anos')
+
+    var m1 = Number(n1.value)
+    var m2 = Number(n2.value)
+    var m3 = Number(n3.value)
+
+    var N1 = document.querySelector('.imprimir1')
+    var N2 = document.querySelector('.imprimir2')
+    var N3 = document.querySelector('.imprimir3')
+
+    N1.innerHTML(n1.value)
+    N2.innerHTML(n2.value)
+    N3.innerHTML(n3.value)
+}
+
+function clean(){
+         
+    document.getElementById('rest').innerHTML = "" ;
+}
+
+function insert(num){
+
+   var numero = document.querySelector('#rest').innerHTML
+   document.querySelector('#rest').innerHTML = numero + num
+
+}
+
+function back(){
+    var resultado = document.querySelector('#rest').innerHTML
+    document.querySelector('#rest').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function somar(){
+    var resultado = document.querySelector('#rest').innerHTML
+    if(resultado){
+        document.querySelector('#rest').innerHTML = eval(resultado)
+    }
+    else{
+        document.querySelector('#rest').innerHTML = "Nada..."
+    }
+} */
